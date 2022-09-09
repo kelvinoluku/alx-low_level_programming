6a961e8a57fabf197de5bf4d704aacdb1a4f9520
kelvinoluku <kelvinoluku15@gmail.com>
2022-09-09 16:52:02 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,19 +15,20 @@ int main(void)
 
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+		if (n > 0)
 	{
 		printf("%i is positive\n", n);
 	}
-	else if (n == 0)
+			else if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
-	else
+				else
 	{
 		printf("%i is negative\n", n);
 	}
-	return (0);
+					return (0);
 }
