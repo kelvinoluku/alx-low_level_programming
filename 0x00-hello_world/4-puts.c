15d470301bd14c969e5daa914119ca3be0c127c5
kelvinoluku <kelvinoluku15@gmail.com>
2022-09-08 21:15:51 -0700

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-   /*
-    *main-Entry point
-    *Return: Always 0 (Success)
+   /**
+    **main-Entry point
+    **
+    **Return: Always 0 (Success)
+    *
+    *
     */
 int main(void)
 {
