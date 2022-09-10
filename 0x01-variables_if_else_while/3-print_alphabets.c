874415591b3be236b8971e1c71d27f3f9b0dbbd4
kelvinoluku <kelvinoluku15@gmail.com>
2022-09-10 04:51:20 -0700

    task 3 correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,56 +2,30 @@
 
 
 
-
-
-/*
- *
+/**
  * main - Entry point
- *
- *Return: Always 0 (Success)
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 
 
-
 {
+char l;
+char j;
+l = 'a';
+j = 'A';
+while (l <= 'z')
+{
+putchar(l);
+l++;
 
-	char l;
-
-	char j;
-
-	l = 'a';
-
-	j = 'A';
-
-	while (l <= 'z')
-
-	{
-
-		putchar(l);
-
-		l++;
-
-
-
-	}
-
-	while (j <= 'Z')
-
-	{
-
-		putchar(j);
-
-		j++;
-
-	}
-
-	putchar('\n');
-
-	return (0);
 
 }
-
-
+while (j <= 'Z')
+{
+putchar(j);
+j++;
+}
+putchar('\n');
+return (0);
+}
