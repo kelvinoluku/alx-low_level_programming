3d31cf4a349fa874c3e699bf50009045fa1b5878
kelvinoluku <kelvinoluku15@gmail.com>
2022-09-10 05:08:19 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * code description
+ * Return: Always 0 (Success)
+ */
+{
+	char j;
+
+	j = 'z';
+
+	while (j >= 'a')
+
+
+{
+		putchar(j);
+		j--;
+}
+	putchar('\n');
+	return (0);
+}
