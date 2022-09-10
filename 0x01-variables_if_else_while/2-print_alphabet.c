7ae9f4d4d631400ed0197f0ecd9872c52d331688
kelvinoluku <kelvinoluku15@gmail.com>
2022-09-10 04:39:18 -0700

    correction 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,26 +3,31 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 
 int main(void)
 {
+
+
 	char q;
 
+
 	q = 'a';
+
 
 	while (q <= 'z')
 {
 
 
 	putchar(q);
-	
-	
+
+
 	q++;
 }
+
 
 putchar('\n');
 return (0);
