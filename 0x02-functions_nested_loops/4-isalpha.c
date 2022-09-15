bab99374c516c2043d742d7bb2f02c96a5d00de9
kelvinoluku <kelvinoluku15@gmail.com>
2022-09-14 18:41:18 -0700

    Task 4 correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- *  _isalpha - function that checks for alphabetic character.
- *  @c: paramter member c
- *  Return: returns 1 or 0 if success
+ * _isalpha - function that checks for alphabetic character.
+ * @c: paramter member c
+ * Return: returns 1 or 0 if success
  */
 
 int _isalpha(int c)
 {
-
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 
 }
