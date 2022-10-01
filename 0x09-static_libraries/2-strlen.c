437652393d5a4ada6421004c4fa27d7ea8bef234
kelvinoluku <kelvinoluku15@gmail.com>
2022-09-30 21:16:20 -0700

    Task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+
+ * _strlen - returns the lenght of string
+
+ * @s: string to find length
+
+ * Return: length of string
+
+ */
+
+
+
+int _strlen(char *s)
+
+{
+
+	int m = 0;
+
+
+
+	while (*(s + m))
+
+		m++;
+
+	return (m);
+
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+
+ * _isalpha - function that checks for alphabetic character.
+
+ * @c: paramter member c
+
+ * Return: returns 1 or 0 if success
+
+ */
+
+
+
+int _isalpha(int c)
+
+{
+
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+
+
+}
